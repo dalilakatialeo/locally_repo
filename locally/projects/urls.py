6e983c57e56e327aa55e51b38339e024d54be6ea
Dalila K Leo <dalilakatialeo@gmail.com>
2021-09-11 11:57:04 +1000

    project setup

---
+++ locally/projects/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('projects/', views.ProjectList.as_view()),
+    ]
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ locally/projects/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Project
+from .serializers import ProjectSerializer
+
+# Create your views here.
+
+class ProjectList(APIView):
+
+    def get(self, request):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
