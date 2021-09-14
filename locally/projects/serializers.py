30631e57cf60fe260199299ed1c48dcc4bc23ffc
Dalila K Leo <dalilakatialeo@gmail.com>
2021-09-14 20:28:13 +1000

    user authentication

--- locally/projects/serializers.py
+++ locally/projects/serializers.py
@@ -31,3 +31,17 @@ class ProjectSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Project.objects.create(**validated_data)
 
+class ProjectDetailSerializer(ProjectSerializer):
+    donations = DonationSerializer(many=True, read_only=True)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.location = validated_data.get('location', instance.location)
+        instance.donation = validated_data.get('donation', instance.donation)
+        instance.image = validated_data.get('image', instance.image)
+        instance.is_open = validated_data.get('is_open', instance.is_open)
+        instance.date_created = validated_data.get('date_created', instance.date_created)
+        instance.owner = validated_data.get('owner', instance.owner)
+        return instance
+        
--- locally/projects/views.py
+++ locally/projects/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Donation
-from .serializers import ProjectSerializer, DonationSerializer
+from .serializers import ProjectDetailSerializer, ProjectSerializer, DonationSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
 
 # Create your views here.
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         projects = Project.objects.all()
@@ -26,7 +27,6 @@ class ProjectList(APIView):
             serializer.errors,status=status.HTTP_400_BAD_REQUEST
             )
 
-
 class ProjectDetail(APIView):
     def get_object(self, pk):
         try:
@@ -37,6 +37,18 @@ class ProjectDetail(APIView):
     def get(self, request, pk):
         project = self.get_object(pk)
         serializer = ProjectSerializer(project)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        data = request.data
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
         return Response(serializer.data)
 
 class DonationList(APIView):
