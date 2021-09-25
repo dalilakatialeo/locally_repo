5dce64bc4429af3787f602582ae145b6553313bb
Dalila K Leo <dalilakatialeo@gmail.com>
2021-09-25 11:14:18 +1000

    migrations

--- locally/projects/migrations/0012_project_goal.py
+++ locally/projects/migrations/0012_project_goal.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0011_remove_project_goal'),
+        ('projects', '0010_project_donation'),
     ]
 
     operations = [
